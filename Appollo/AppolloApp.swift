1ec2157a6333946eb5e1c126722f7d1ec9c96a34
Ivan <ivan.yarmoliuk@gmail.com>
2023-06-08 19:27:53 -0700

    Initial Commit

---
+++ Appollo/AppolloApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppolloApp.swift
+//  Appollo
+//
+//  Created by Ivan Yarmoliuk on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct AppolloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
